e2cea6b3689194f2a0853c52dbc6557f9ef810f5
Patricia Novaski Gavloski <128609847+PatriciaGavloski@users.noreply.github.com>
2024-05-21 14:24:26 -0300

    Add files via upload

--- Java/src/Estudos/Aulas/Aula04.java
+++ Java/src/Estudos/Aulas/Aula04.java
@@ -1,5 +1,17 @@
 package Estudos.Aulas;
 
 public class Aula04 {
-    
+
+    public static void main(String[] args) throws Exception {
+        
+        int numero = 1;
+
+        numero = 2; 
+
+        final double VALOR_PI = 3.14; 
+        
+        /** Variavel final não pode mudar valor → por convenção usar as contantes com LETRAS MAIUSCULA */
+
+        System.out.println(VALOR_PI);
+    }
 }
---
+++ Java/src/Estudos/Aulas/Aula05.java
@@ -0,0 +1,5 @@
+package Estudos.Aulas;
+
+public class Aula05 {
+    
+}
