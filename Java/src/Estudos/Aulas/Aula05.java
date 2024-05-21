1a770207933fb24ab5c26e5508e2d7bf8528adba
Patricia Novaski Gavloski <128609847+PatriciaGavloski@users.noreply.github.com>
2024-05-21 14:44:25 -0300

    Add files via upload

--- Java/src/Estudos/Aulas/Aula05.java
+++ Java/src/Estudos/Aulas/Aula05.java
@@ -2,4 +2,35 @@ package Estudos.Aulas;
 
 public class Aula05 {
     
+    /* Operadores */
+
+    public static void main (String[] args){
+
+        String nomeCompleto = "Linguagem" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        
+        System.out.println(concatenacao);
+
+
+
+    }
 }
