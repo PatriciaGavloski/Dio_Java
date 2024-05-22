96bb4e16219964f218de30474176400b79873da4
Patricia Novaski Gavloski <128609847+PatriciaGavloski@users.noreply.github.com>
2024-05-22 13:03:42 -0300

    Add files via upload

---
+++ Java/src/Estudos/Aulas/Aula09.java
@@ -0,0 +1,34 @@
+package Estudos.Aulas;
+
+public class Aula09 {
+    public static void main (String [] args) {
+        /* Operadores Relacionais */
+
+        String nomeUm = "Patricia";
+        String nomeDois = "Patricia";
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+                System.out.println("A Nossa condição é Verdadeira");
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é Diferente de numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("numeroUm é Maior que numeroDois? " + simNao);
+
+
+
+    }
+}
---
+++ Java/src/Estudos/Aulas/Aula10.java
@@ -0,0 +1,22 @@
+package Estudos.Aulas;
+
+public class Aula10 {
+    public static void main (String [] args) {
+        /* Operadores Lógicos */
+
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("Fim");
+
+    }
+    
+}
