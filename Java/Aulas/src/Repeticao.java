715f63caaebe2d50f49e032e9209d108e21cd01c
Patricia Novaski Gavloski <128609847+PatriciaGavloski@users.noreply.github.com>
2024-06-25 09:00:29 -0300

    Add files via upload

---
+++ Java/Aulas/src/Repeticao.java
@@ -0,0 +1,12 @@
+public class Repeticao {
+    public static void main(String[] args) {
+
+         for (int carneirinhos = 1; carneirinhos <=20; carneirinhos ++){
+            System.out.println("Contando Carneirinhos " + carneirinhos );
+         }
+
+         System.out.println("zZzZzZz...Joãozinho dormiu ");
+        
+    }
+    
+}
